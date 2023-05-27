b56ac5d8dde78cb8bc505dd4dae3f2af2a3228e6
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-05-27 18:45:21 -0400

    Update login.js

--- login.js
+++ login.js
@@ -9,5 +9,19 @@ const cookie = "ez"
 app.use(express.static("public"));
 
 async function(){=
-  await rbx.setCookie(cookie)
+  await rbx.setCookie(cookie);
+  let currentUser = await rbx.getCurrentUser();
+  console.log(currentUser.UserName);
 }
+
+app.get("/ranker", (req, res) => {
+    var User = req.param("userid");
+    var Rank = req.param("rank");
+  
+    rbx.setRank(groupId, parseInt(User), parseInt(Rank));
+    res.json("Ranked!");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
