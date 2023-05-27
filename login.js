6faa8e09efbd3a1c40b07f7ae2ce68925a8c321a
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-05-27 18:43:38 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const rbx = require("noblox.js");
+const app = express();
 
+app.use(express.static("public"));
+
+const cookie = "ez"
+
+app.use(express.static("public"));
+
+async function(){=
+  await rbx.setCookie(cookie)
+}
